7095e78d54122743cdb2ecea00c0bdccecb94d75
Chris Deimert <chris.deimert@gmail.com>
2015-01-13 17:44:48 -0700

    Added chi2 to Lab 1

--- Lab1/Lab1_main.f90
+++ Lab1/Lab1_main.f90
@@ -8,6 +8,8 @@ program Lab1_main
     call lcm_test(a = 107, c = 1283, m = 6075, seed = 1, test_num = 2)
     call lcm_test(a = 1103515245, c = 12345, m = 32768, seed = 1, test_num = 3)
 
+    call lcm_chi2_test(a = 106, c = 1283, m = 6075, seed = 1)
+
 contains
 
     subroutine lcm_test(a, c, m, seed, test_num)
@@ -31,6 +33,49 @@ contains
 
         close(file_id)
 
+    end subroutine
+
+    subroutine lcm_chi2_test(a, c, m, seed)
+        integer, intent(in) :: a, c, m, seed
+        integer, dimension(2,10) :: lcm_table
+        integer, dimension(2,10) :: gfort_table
+        real(8) :: lcm_rand, gfort_rand
+        real(8) :: lcm_chi2, gfort_chi2
+        integer :: file_id, i, j
+
+        open(unit = new_file_unit(file_id), file = "./Data/LCM_chi2_test.txt", action = 'write')
+
+        call set_lcm_params(a_val = a, c_val = c, m_val = m)
+        call set_lcm_seed(seed_val = seed)
+
+        lcm_table = 0.0
+        gfort_table = 0.0
+        do i = 1, 100
+            lcm_rand = lcm_random_number()
+            call random_number(gfort_rand)
+            do j = 1, 10
+                if ((lcm_rand >= (j-1)*0.1) .and. (lcm_rand < j*0.1)) then
+                    lcm_table(1,j) = lcm_table(1,j) + 1
+                end if
+                if ((gfort_rand >= (j-1)*0.1) .and. (gfort_rand < j*0.1)) then
+                    gfort_table(1,j) = gfort_table(1,j) + 1
+                end if
+            end do
+        end do
+        lcm_table(2,:) = 10
+        gfort_table(2,:) = 10
+
+        do i = 1, 10
+            write(file_id,*) lcm_table(1,i), gfort_table(1,i), lcm_table(2,i)
+        end do
+
+        lcm_chi2 = calculate_chi2(real(lcm_table, kind = 8))
+        gfort_chi2 = calculate_chi2(real(gfort_table, kind = 8))
+
+        write(*,*) lcm_chi2, gfort_chi2
+
+        close(file_id)
+        
     end subroutine
     
 end program
--- Modules/Useful_stuff_module.f90
+++ Modules/Useful_stuff_module.f90
@@ -2,22 +2,76 @@ module Useful_stuff_module
     implicit none
     private
 
-    public :: new_file_unit
+    public :: new_file_unit, calculate_chi2
 
 contains
-    integer function new_file_unit(unit) result(n)
-        integer, intent(out), optional :: unit
-        logical inuse
-        integer, parameter :: n_min = 10
-        integer, parameter :: n_max = 999
 
-        do n = n_min, n_max
-            inquire(unit = n, opened = inuse)
-            if (.not. inuse) then
-                if (present(unit)) unit = n
-                return
-            end if
-        end do
-        write(*,*) "new_unit ERROR: available unit not found."
-    end function
+real(8) function calculate_chi2(obs_freq)
+    real(8), dimension(:,:), intent(in) :: obs_freq
+    real(8), dimension(:,:), allocatable :: exp_freq
+    integer, dimension(2) :: dims
+    integer :: n, m
+    real(8), dimension(:,:), allocatable :: row_marg_tot
+    real(8), dimension(:,:), allocatable :: col_marg_tot
+    real(8) :: chi2
+    real(8) :: grand_total
+    integer :: i, j 
+
+    dims = shape(obs_freq)
+    n = dims(1)
+    m = dims(2)
+    allocate(exp_freq(n,m))
+    allocate(row_marg_tot(n,1), col_marg_tot(1,m))
+
+    do i = 1, n
+        row_marg_tot(i,1) = 0.0
+        do j = 1,m
+            row_marg_tot(i,1) = row_marg_tot(i,1) + obs_freq(i,j)
+        enddo
+    enddo
+
+    do i = 1, m
+        col_marg_tot(1,i) = 0.0
+        do j = 1,n
+            col_marg_tot(1,i) = col_marg_tot(1,i) + obs_freq(j,i)
+        enddo
+    enddo
+
+    grand_total = 0.0
+
+    do i = 1, n
+        do j = 1, m
+            grand_total = grand_total + obs_freq(i,j)
+        enddo
+    enddo
+
+    exp_freq = matmul(row_marg_tot, col_marg_tot)/grand_total
+
+    chi2 = 0.0
+    do i = 1, n
+        do j = 1, m
+            chi2 = chi2 + ((obs_freq(i,j) - exp_freq(i,j))**(2.0))/exp_freq(i,j)
+        enddo
+    enddo
+
+    calculate_chi2 = chi2
+
+end function
+
+integer function new_file_unit(unit) result(n)
+    integer, intent(out), optional :: unit
+    logical inuse
+    integer, parameter :: n_min = 10
+    integer, parameter :: n_max = 999
+
+    do n = n_min, n_max
+        inquire(unit = n, opened = inuse)
+        if (.not. inuse) then
+            if (present(unit)) unit = n
+            return
+        end if
+    end do
+    write(*,*) "new_unit ERROR: available unit not found."
+end function
+
 end module
