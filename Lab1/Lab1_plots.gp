b03517365aa7ca64bc6fafe78c83dab65e3a35b6
Chris Deimert <chris.deimert@gmail.com>
2015-01-24 17:09:17 -0700

    Started radiative transfer stuff

--- Lab1/Lab1_plots.gp
+++ Lab1/Lab1_plots.gp
@@ -28,4 +28,33 @@ unset key
 
 plot "./Data/LCM_test_3.txt" with points pt 0 lc 0
 
+set output "./Plots/Tau_histogram.pdf"
+
+set title "Tau histogram"
+set xlabel "tau"
+set ylabel ""
+unset key
+
+plot "./Data/Tau_histogram.txt" with boxes
+
+set output "./Plots/Mu_histogram.pdf"
+
+set title "Mu histogram"
+set xlabel "mu"
+set ylabel ""
+set yrange[0:12000]
+unset key
+
+plot "./Data/Mu_histogram.txt" with boxes
+
+set output "./Plots/Phi_histogram.pdf"
+
+set title "Phi histogram"
+set xlabel "phi"
+set ylabel ""
+set yrange[0:12000]
+unset key
+
+plot "./Data/Phi_histogram.txt" with boxes
+
 reset
---
+++ Lab1/Radiative_transfer.f90
@@ -0,0 +1,76 @@
+program Lab1_main
+    use Useful_stuff_module
+    use Random_numbers_module
+    use Ouyed_random_number_module
+    
+    implicit none
+
+    real(8), parameter :: pi = 4.0d0*atan(1.0d0)
+
+    real(8), dimension(1000000) :: tau, mu, phi
+    real(8), dimension(100) :: tau_bins, mu_bins, phi_bins
+    integer, dimension(100) :: tau_hist, mu_hist, phi_hist
+    integer :: tau_id, mu_id, phi_id
+    integer :: i
+
+    open(unit = new_file_unit(tau_id), file = "./Data/Tau_histogram.txt", action = 'write')
+    open(unit = new_file_unit(mu_id), file = "./Data/Mu_histogram.txt", action = 'write')
+    open(unit = new_file_unit(phi_id), file = "./Data/Phi_histogram.txt", action = 'write')
+    
+    !call set_up_lcm_generator()
+    call init_random_seed()
+
+    ! Run histogram tests
+
+    do i = 1, size(tau)
+        tau(i) = random_tau()
+        mu(i) = random_mu()
+        phi(i) = random_phi()
+    end do
+
+    call histogram(tau, tau_bins, tau_hist)
+    call histogram(mu, mu_bins, mu_hist)
+    call histogram(phi, phi_bins, phi_hist)
+
+    do i = 1, size(tau_hist)
+        write(tau_id, *) tau_bins(i), tau_hist(i)
+        write(mu_id, *) mu_bins(i), mu_hist(i)
+        write(phi_id, *) phi_bins(i), phi_hist(i)
+    end do
+
+contains
+
+    subroutine set_up_lcm_generator()
+        integer(16) :: a, c, m, seed
+
+        a = 1103515245
+        c = 12345
+        m = 32768
+        seed = 1
+
+        call set_lcm_params(a_val = a, c_val = c, m_val = m)
+        call set_lcm_seed(seed_val = seed)
+    end subroutine
+
+    real(8) function random_tau()
+        real(8) :: uni_rand
+        !uni_rand = lcm_random_number()
+        call random_number(uni_rand)
+        random_tau = -log(1.0-uni_rand)
+    end function
+
+    real(8) function random_mu()
+        real(8) :: uni_rand
+        !uni_rand = lcm_random_number()
+        call random_number(uni_rand)
+        random_mu = 1 - 2*uni_rand
+    end function
+
+    real(8) function random_phi()
+        real(8) :: uni_rand
+        !uni_rand = lcm_random_number()
+        call random_number(uni_rand)
+        random_phi = 2*pi*uni_rand
+    end function
+
+end program
