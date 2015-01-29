07fd96e7c7929068d085569d1646854c54a3a800
Chris Deimert <chris.deimert@gmail.com>
2015-01-29 14:36:41 -0700

    Radiative transfer updates

--- Lab1/Radiative_transfer.f90
+++ Lab1/Radiative_transfer.f90
@@ -7,15 +7,16 @@ program Lab1_main
 
     real(8), parameter :: pi = 4.0d0*atan(1.0d0)
 
-    real(8), dimension(1000000) :: tau, mu, phi
-    real(8), dimension(100) :: tau_bins, mu_bins, phi_bins
-    integer, dimension(100) :: tau_hist, mu_hist, phi_hist
-    integer :: tau_id, mu_id, phi_id
+    real(8), dimension(1000000) :: tau, mu, phi, theta
+    real(8), dimension(100) :: tau_bins, mu_bins, phi_bins, theta_bins
+    integer, dimension(100) :: tau_hist, mu_hist, phi_hist, theta_hist
+    integer :: tau_id, mu_id, phi_id, theta_id
     integer :: i
 
     open(unit = new_file_unit(tau_id), file = "./Data/Tau_histogram.txt", action = 'write')
     open(unit = new_file_unit(mu_id), file = "./Data/Mu_histogram.txt", action = 'write')
     open(unit = new_file_unit(phi_id), file = "./Data/Phi_histogram.txt", action = 'write')
+    open(unit = new_file_unit(theta_id), file = "./Data/Theta_histogram.txt", action = 'write')
     
     !call set_up_lcm_generator()
     call init_random_seed()
@@ -26,17 +27,25 @@ program Lab1_main
         tau(i) = random_tau()
         mu(i) = random_mu()
         phi(i) = random_phi()
+        theta(i) = acos(mu(i))
     end do
 
     call histogram(tau, tau_bins, tau_hist)
     call histogram(mu, mu_bins, mu_hist)
     call histogram(phi, phi_bins, phi_hist)
+    call histogram(theta, theta_bins, theta_hist)
 
     do i = 1, size(tau_hist)
         write(tau_id, *) tau_bins(i), tau_hist(i)
         write(mu_id, *) mu_bins(i), mu_hist(i)
         write(phi_id, *) phi_bins(i), phi_hist(i)
+        write(theta_id, *) theta_bins(i), theta_hist(i)
     end do
+
+    close(tau_id)
+    close(mu_id)
+    close(phi_id)
+    close(theta_id)
 
 contains
 
--- Lab1/Radiative_transfer.gp
+++ Lab1/Radiative_transfer.gp
@@ -15,6 +15,7 @@ set output "./Plots/Mu_histogram.pdf"
 set title "Mu histogram"
 set xlabel "mu"
 set ylabel ""
+set xrange[-1:1]
 set yrange[0:12000]
 unset key
 
@@ -25,9 +26,21 @@ set output "./Plots/Phi_histogram.pdf"
 set title "Phi histogram"
 set xlabel "phi"
 set ylabel ""
+set xrange[0:6.3]
 set yrange[0:12000]
 unset key
 
 plot "./Data/Phi_histogram.txt" with boxes
+
+set output "./Plots/Theta_histogram.pdf"
+
+set title "Theta histogram"
+set xlabel "theta"
+set ylabel ""
+set xrange[0:3.2]
+set yrange[0:17000]
+unset key
+
+plot "./Data/Theta_histogram.txt" with boxes
 
 reset
