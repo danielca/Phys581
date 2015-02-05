860ce9f53d48168bbe6eaf21ff0b524573e26d42
Chris Deimert <chris.deimert@gmail.com>
2015-02-04 20:14:51 -0700

    Updated radiative transfer

--- Lab1/Radiative_transfer.f90
+++ Lab1/Radiative_transfer.f90
@@ -1,54 +1,48 @@
-program Lab1_main
+program Radiative_transfer
     use Useful_stuff_module
     use Random_numbers_module
     use Ouyed_random_number_module
-    
+
     implicit none
 
+    type photon
+        real(8) :: phi, theta
+        real(8) :: x, y, z
+        integer :: state, life_time
+    end type
+
     real(8), parameter :: pi = 4.0d0*atan(1.0d0)
+    integer, parameter :: ALIVE = 0
+    integer, parameter :: ABSORBED = 1
+    integer, parameter :: REFLECTED = 2
+    integer, parameter :: TRANSMITTED = 3
 
-    real(8), dimension(1000000) :: tau, mu, phi, theta
-    real(8), dimension(100) :: tau_bins, mu_bins, phi_bins, theta_bins
-    integer, dimension(100) :: tau_hist, mu_hist, phi_hist, theta_hist
-    integer :: tau_id, mu_id, phi_id, theta_id
-    integer :: i
+    character(len=1024) :: filename
 
-    open(unit = new_file_unit(tau_id), file = "./Data/Tau_histogram.txt", action = 'write')
-    open(unit = new_file_unit(mu_id), file = "./Data/Mu_histogram.txt", action = 'write')
-    open(unit = new_file_unit(phi_id), file = "./Data/Phi_histogram.txt", action = 'write')
-    open(unit = new_file_unit(theta_id), file = "./Data/Theta_histogram.txt", action = 'write')
-    
-    !call set_up_lcm_generator()
-    call init_random_seed()
+    call generate_histograms()
 
-    ! Run histogram tests
+    write(filename, "(a34)") "./Data/Scattering_experiment_1.txt"
+    call scattering_experiment( tau_max = 10.0d0,             &
+                                z_max = 1.0d0,                &
+                                prob_abs = -0.1d0,             &
+                                num_photons = 100000,         &
+                                output_file = filename )
 
-    do i = 1, size(tau)
-        tau(i) = random_tau()
-        mu(i) = random_mu()
-        phi(i) = random_phi()
-        theta(i) = acos(mu(i))
-    end do
+    write(filename, "(a34)") "./Data/Scattering_experiment_2.txt"
+    call scattering_experiment( tau_max = 10.0d0,             &
+                                z_max = 1.0d0,                &
+                                prob_abs = 0.5d0,             &
+                                num_photons = 100000,         &
+                                output_file = filename )
 
-    call histogram(tau, tau_bins, tau_hist)
-    call histogram(mu, mu_bins, mu_hist)
-    call histogram(phi, phi_bins, phi_hist)
-    call histogram(theta, theta_bins, theta_hist)
+    write(filename, "(a35)") "./Data/Single_photon_experiment.txt"
+    call single_photon_experiment( tau_max = 10.0d0,             &
+                                   z_max = 1.0d0,                &
+                                   prob_abs = -0.1d0,             &
+                                   output_file = filename )
 
-    do i = 1, size(tau_hist)
-        write(tau_id, *) tau_bins(i), tau_hist(i)
-        write(mu_id, *) mu_bins(i), mu_hist(i)
-        write(phi_id, *) phi_bins(i), phi_hist(i)
-        write(theta_id, *) theta_bins(i), theta_hist(i)
-    end do
-
-    close(tau_id)
-    close(mu_id)
-    close(phi_id)
-    close(theta_id)
 
 contains
-
     subroutine set_up_lcm_generator()
         integer(16) :: a, c, m, seed
 
@@ -72,14 +66,229 @@ contains
         real(8) :: uni_rand
         !uni_rand = lcm_random_number()
         call random_number(uni_rand)
-        random_mu = 1 - 2*uni_rand
+        random_mu = 1.0 - 2.0*uni_rand
+    end function
+
+    real(8) function random_theta()
+        random_theta = acos(random_mu())
     end function
 
     real(8) function random_phi()
         real(8) :: uni_rand
         !uni_rand = lcm_random_number()
         call random_number(uni_rand)
-        random_phi = 2*pi*uni_rand
+        random_phi = 2.0*pi*uni_rand
     end function
+
+    subroutine init_photons(photons)
+        type (photon), dimension(:), intent(inout) :: photons
+        real(8) :: mu
+        integer :: i
+
+        do i = 1, size(photons)
+            mu = random_mu()
+            do while (mu < 0.0)
+                mu = random_mu()    ! Ensure mu is positive initially
+            end do
+
+            photons(i)%theta = acos(mu)
+            photons(i)%phi = random_phi()
+            photons(i)%x = 0.0
+            photons(i)%y = 0.0
+            photons(i)%z = 0.0
+            photons(i)%state = ALIVE
+            photons(i)%life_time = 1
+        end do
+    end subroutine
+
+    integer function num_in_state(photons, state)
+        type (photon), dimension(:), intent(in) :: photons
+        integer, intent(in) :: state
+        integer :: i
+
+        num_in_state = 0
+        do i = 1, size(photons)
+            if (photons(i)%state == state) then
+                num_in_state = num_in_state + 1
+            end if
+        end do
+    end function
+
+    subroutine step_forward(tau_max, z_max, prob_abs, photons)
+        real(8), intent(in) :: tau_max, z_max, prob_abs
+        type (photon), dimension(:), intent(inout) :: photons
+        integer :: num_photons, i
+        real(8) :: tau, dist, uni_rand
+
+        num_photons = size(photons)
+
+        do i = 1, num_photons
+            if (photons(i)%state == ALIVE) then
+                dist = random_tau()*z_max/tau_max
+                photons(i)%x = photons(i)%x + dist*sin(photons(i)%theta)*cos(photons(i)%phi)
+                photons(i)%y = photons(i)%y + dist*sin(photons(i)%theta)*sin(photons(i)%phi)
+                photons(i)%z = photons(i)%z + dist*cos(photons(i)%theta)
+
+                photons(i)%life_time = photons(i)%life_time + 1
+
+                call random_number(uni_rand)
+                if (photons(i)%z > z_max) then
+                    photons(i)%state = TRANSMITTED
+                else if (photons(i)%z < 0.0) then
+                    photons(i)%state = REFLECTED
+                else if (uni_rand < prob_abs) then
+                    photons(i)%state = ABSORBED
+                else
+                    photons(i)%theta = random_theta()
+                    photons(i)%phi = random_phi()
+                end if
+            end if
+        end do
+
+    end subroutine
+
+    subroutine generate_histograms()
+        real(8), dimension(1000000) :: tau, mu, phi, theta
+        real(8), dimension(100) :: tau_bins, mu_bins, phi_bins, theta_bins
+        integer, dimension(100) :: tau_hist, mu_hist, phi_hist, theta_hist
+        integer :: tau_id, mu_id, phi_id, theta_id
+        integer :: i
+
+        open(unit = new_file_unit(tau_id), file = "./Data/Tau_histogram.txt", action = 'write')
+        open(unit = new_file_unit(mu_id), file = "./Data/Mu_histogram.txt", action = 'write')
+        open(unit = new_file_unit(phi_id), file = "./Data/Phi_histogram.txt", action = 'write')
+        open(unit = new_file_unit(theta_id), file = "./Data/Theta_histogram.txt", action = 'write')
+        
+        !call set_up_lcm_generator()
+        call init_random_seed()
+
+        ! Run histogram tests
+
+        do i = 1, size(tau)
+            tau(i) = random_tau()
+            mu(i) = random_mu()
+            phi(i) = random_phi()
+            theta(i) = acos(mu(i))
+        end do
+
+        call histogram(tau, tau_bins, tau_hist)
+        call histogram(mu, mu_bins, mu_hist)
+        call histogram(phi, phi_bins, phi_hist)
+        call histogram(theta, theta_bins, theta_hist)
+
+        do i = 1, size(tau_hist)
+            write(tau_id, *) tau_bins(i), tau_hist(i)
+            write(mu_id, *) mu_bins(i), mu_hist(i)
+            write(phi_id, *) phi_bins(i), phi_hist(i)
+            write(theta_id, *) theta_bins(i), theta_hist(i)
+        end do
+
+        close(tau_id)
+        close(mu_id)
+        close(phi_id)
+        close(theta_id)
+
+    end subroutine
+
+    subroutine scattering_experiment(tau_max, z_max, prob_abs, num_photons, output_file)
+        real(8), intent(in) :: tau_max, z_max, prob_abs
+        character(len = 1024), intent(in) :: output_file
+        integer, intent(in) :: num_photons
+        type (photon), dimension(num_photons) :: photons
+        integer :: file_id, num_iter, max_num_iter, i
+
+        open( unit = new_file_unit(file_id), file = output_file, action = 'write' )
+
+        max_num_iter = 100000
+        call init_photons(photons)
+
+        num_iter = 0
+        do while (num_in_state(photons, ALIVE) > 0)
+            call step_forward(tau_max, z_max, prob_abs, photons)
+            num_iter = num_iter + 1
+            if (num_iter > max_num_iter) then
+                write(*,*) "WARNING in scattering_experiment: too many iterations!"
+                exit
+            end if
+        end do
+
+        do i = 1, num_photons
+            write(file_id, *) photons(i)%x, photons(i)%y, photons(i)%z,     &
+                              photons(i)%theta, photons(i)%phi,             &
+                              photons(i)%state, photons(i)%life_time
+        end do
+
+        write(*,*) "================================"
+        write(*,*) "Scattering experiment:"
+        write(*,*) ""
+        write(*,*) "tau_max        = ", tau_max
+        write(*,*) "z_max          = ", z_max
+        write(*,*) "prob_abs       = ", prob_abs
+        write(*,*) "num_photons    = ", num_photons
+        write(*,*) "num_iterations = ", num_iter
+        write(*,*) ""
+        write(*,*) "Output file    = ", trim(output_file)
+        write(*,*) "================================"
+
+        close(file_id)
+
+    end subroutine
+
+    subroutine single_photon_experiment(tau_max, z_max, prob_abs, output_file)
+        real(8), intent(in) :: tau_max, z_max, prob_abs
+        character(len = 1024), intent(in) :: output_file
+        type (photon), dimension(1) :: photons
+        real(8), dimension(100000) :: x, y, z
+        integer :: file_id, num_iter, max_num_iter, i
+
+        open( unit = new_file_unit(file_id), file = output_file, action = 'write' )
+
+        max_num_iter = 100000
+
+        call init_photons(photons)
+        do while (photons(1)%life_time < 700)
+
+            call init_photons(photons)
+
+            do while (num_in_state(photons, ALIVE) > 0)
+                x(photons(1)%life_time) = photons(1)%x
+                y(photons(1)%life_time) = photons(1)%y
+                z(photons(1)%life_time) = photons(1)%z
+
+                call step_forward(tau_max, z_max, prob_abs, photons)
+                if (photons(1)%life_time > max_num_iter) then
+                    write(*,*) "WARNING in scattering_experiment: too many iterations!"
+                    exit
+                end if
+
+            end do
+
+            x(photons(1)%life_time) = photons(1)%x
+            y(photons(1)%life_time) = photons(1)%y
+            z(photons(1)%life_time) = photons(1)%z
+
+        end do
+
+        do i = 1, photons(1)%life_time 
+            write(file_id, *) x(i), y(i), z(i), i
+        end do
+
+        write(*,*) "================================"
+        write(*,*) "Single photon experiment:"
+        write(*,*) ""
+        write(*,*) "tau_max        = ", tau_max
+        write(*,*) "z_max          = ", z_max
+        write(*,*) "prob_abs       = ", prob_abs
+        write(*,*) ""
+        write(*,*) "Output file    = ", trim(output_file)
+        write(*,*) ""
+        write(*,*) "Photon life    = ", photons(1)%life_time
+        write(*,*) "Photon state   = ", photons(1)%state
+        write(*,*) ""
+        write(*,*) "================================"
+
+        close(file_id)
+
+    end subroutine
 
 end program
